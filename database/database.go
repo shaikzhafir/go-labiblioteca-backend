72cfcf091802c31be6542cf463825e0e15955224
shaik-zhafir <shaik_zhafir@dell.com>
2021-09-28 23:03:59 +0800

    separated db logic to database.go

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
@@ -9,15 +10,23 @@ import (
 
 var DbConn *sql.DB
 
-func SetupDatabase() {
+func ConnectDatabase() *sql.DB {
 	var err error
 	//sql.Open creates a pointer to Db (*Db)
-	DbConn, err := sql.Open("postgres", "postgres://poop@localhost/bookdb?sslmode=disable")
+	DbConn, err := sql.Open("postgres", "postgres://postgres:poop@localhost/bookdb?sslmode=disable")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	DbConn.SetMaxOpenConns(3)
+	err = DbConn.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected")
+
+	return DbConn
 
 }
--- handlers/hello.go
+++ handlers/hello.go
@@ -1,14 +1,14 @@
 package handlers
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"go-labiblioteca-backend/data"
+	"go-labiblioteca-backend/database"
 	"log"
 	"net/http"
-)
 
+	_ "github.com/lib/pq"
+)
 
 type HelloHandler struct {
 	Message string
@@ -19,30 +19,24 @@ func Hello() *HelloHandler {
 	return &HelloHandler{Message: "POOP"}
 }
 
-func (h *HelloHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request){
+func (h *HelloHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	//convert the string message to bytes
 	var err error
 	//sql.Open creates a pointer to Db (*Db)
-	DbConn, err := sql.Open("postgres", "postgres://postgres:poop@localhost/bookdb?sslmode=disable")
-
+	DbConn := database.ConnectDatabase()
+	rows, err := DbConn.Query("SELECT * FROM books;")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	DbConn.SetMaxOpenConns(3)
-	rows,err := DbConn.Query("SELECT * FROM books;")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bks := make([]data.Book,0)
+	bks := make([]data.Book, 0)
 	for rows.Next() {
 		bk := data.Book{}
-		if err = rows.Scan(&bk.Isbn,&bk.Title,&bk.Description,&bk.Author,&bk.Image);err != nil {
+		if err = rows.Scan(&bk.Isbn, &bk.Title, &bk.Description, &bk.Author, &bk.Image); err != nil {
 			log.Fatal(err)
 		}
-		bks = append(bks,bk)
+		bks = append(bks, bk)
 		fmt.Println(bks)
 
 	}
-	}
+}
