b786e9b322236f17b35b01078efd00b025408a6c
shaik-zhafir <shaik_zhafir@dell.com>
2021-09-28 23:05:32 +0800

    playing arnd

--- database/database.go
+++ database/database.go
@@ -12,13 +12,14 @@ var DbConn *sql.DB
 
 func ConnectDatabase() *sql.DB {
 	var err error
+	var midget string = "midget"
 	//sql.Open creates a pointer to Db (*Db)
 	DbConn, err := sql.Open("postgres", "postgres://postgres:poop@localhost/bookdb?sslmode=disable")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	fmt.Println(midget)
 	err = DbConn.Ping()
 
 	if err != nil {
